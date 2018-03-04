2bf92c9baa82a71779a72b5e76da769aaa4717b1
Daniel Brown <fireflip@gmail.com>
2018-03-04 14:49:21 -0600

    Update gulpfile to show sass error

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ gulp.task('watch', function(){
 
 gulp.task('sass', function(){
   return gulp.src('library/scss/**/*.scss')
-    .pipe(sass()) // Using gulp-sass
     .pipe(sass().on('error', sass.logError))
     .pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
     .pipe(gulp.dest('library/css'))
